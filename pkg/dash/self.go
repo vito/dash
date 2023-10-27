4c8415a0b55ea28155f435f20f73c555de9dfc79
Alex Suraci <suraci.alex@gmail.com>
2023-10-27 18:29:58 -0400

    typecheck values passed to self()

--- pkg/dash/self.go
+++ pkg/dash/self.go
@@ -1,20 +1,49 @@
 package dash
 
 import (
+	"fmt"
+
 	"github.com/chewxy/hm"
 )
 
-type Self struct{
+type Self struct {
 	// Slots to override.
 	Args Record
 }
 
 var _ Node = Self{}
 
-func (Self) Infer(env hm.Env, fresh hm.Fresher) (hm.Type, error) {
+func (s Self) Infer(env hm.Env, fresh hm.Fresher) (hm.Type, error) {
+	mod := env.(*Module)
+
+	argsType, err := s.Args.Infer(env, fresh)
+	if err != nil {
+		return nil, err
+	}
+
+	argsRec := argsType.(*RecordType)
+
+	for _, f := range argsRec.Fields {
+		haveT, isMono := f.Value.Type()
+		if !isMono {
+			return nil, fmt.Errorf("expected monotype for %s", f.Key)
+		}
+		expectedScheme, found := mod.SchemeOf(f.Key)
+		if !found {
+			return nil, fmt.Errorf("unknown argument: %s", f.Key)
+		}
+		expectedT, isMono := expectedScheme.Type()
+		if !isMono {
+			return nil, fmt.Errorf("expected monotype for %s", f.Key)
+		}
+		if !haveT.Eq(expectedT) {
+			return nil, fmt.Errorf("expected %s to be %s, not %s", f.Key, expectedT, haveT)
+		}
+	}
+
 	// TODO(vito): this used to Clone(), not sure if still
 	// needed or just garbage
-	return NonNullType{env.(*Module)}, nil
+	return NonNullType{mod}, nil
 }
 
 func (s Self) Body() hm.Expression { return s }

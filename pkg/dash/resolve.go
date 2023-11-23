7a02e1459d258d788162bc0ba3fa53fd154b4449
Alex Suraci <suraci.alex@gmail.com>
2023-11-22 19:54:03 -0500

    typecheck args to resolves

--- pkg/dash/resolve.go
+++ pkg/dash/resolve.go
@@ -21,24 +21,84 @@ func (d Resolve) Infer(env hm.Env, fresh hm.Fresher) (hm.Type, error) {
 	}
 	nn, ok := lt.(NonNullType)
 	if !ok {
-		return nil, fmt.Errorf("Select.Infer: expected %T, got %T: %s", nn, lt, lt)
+		return nil, fmt.Errorf("Resolve.Infer: expected %T, got %T: %s", nn, lt, lt)
 	}
 	rec, ok := nn.Type.(*Module)
 	if !ok {
-		return nil, fmt.Errorf("Select.Infer: expected %T, got %T", rec, nn.Type)
+		return nil, fmt.Errorf("Resolve.Infer: expected %T, got %T", rec, nn.Type)
 	}
 	scheme, found := rec.SchemeOf(d.Field)
 	if !found {
-		return nil, fmt.Errorf("Select.Infer: field %q not found in record %s", d.Field, rec)
+		return nil, fmt.Errorf("Resolve.Infer: field %q not found in record %s", d.Field, rec)
 	}
 	t, mono := scheme.Type()
 	if !mono {
-		return nil, fmt.Errorf("Select.Infer: type of field %q is not monomorphic", d.Field)
+		return nil, fmt.Errorf("Resolve.Infer: type of field %q is not monomorphic", d.Field)
 	}
 	switch x := t.(type) {
 	case *hm.FunctionType:
+		definedType := x.Arg()
+
+		inferredType, err := d.Args.Infer(env, fresh)
+		if err != nil {
+			return nil, err
+		}
+
+		definedRecord, ok := definedType.(*RecordType)
+		if !ok {
+			return nil, fmt.Errorf("Resolve.Infer: expected %T, got %T", definedRecord, definedType)
+		}
+
+		inferredRecord, ok := inferredType.(*RecordType)
+		if !ok {
+			return nil, fmt.Errorf("Resolve.Infer: expected %T, got %T", definedRecord, definedType)
+		}
+
+		definedTypes := map[string]*hm.Scheme{}
+		for _, f := range definedRecord.Fields {
+			definedTypes[f.Key] = f.Value
+		}
+
+		inferredTypes := map[string]*hm.Scheme{}
+		for _, f := range inferredRecord.Fields {
+			inferredTypes[f.Key] = f.Value
+		}
+
+		for k, v := range definedTypes {
+			definedT, isMono := v.Type()
+			if !isMono {
+				// TODO should this just be type?
+				return nil, fmt.Errorf("Resolve.Infer: field %q is not monomorphic", k)
+			}
+
+			inferred, ok := inferredTypes[k]
+			if !ok {
+				if _, isRequired := definedT.(NonNullType); isRequired {
+					return nil, fmt.Errorf("Resolve.Infer: field %q is required", k)
+				} else {
+					// optional; skip
+					continue
+				}
+			}
+
+			inferredT, isMono := inferred.Type()
+			if !isMono {
+				// TODO should this just be type?
+				return nil, fmt.Errorf("Resolve.Infer: field %q is not monomorphic", k)
+			}
+
+			if !definedT.Eq(inferredT) {
+				return nil, fmt.Errorf("Resolve.Infer: mismatched types: %s != %s", definedT, inferredT)
+			}
+
+			delete(inferredTypes, k)
+		}
+
+		if len(inferredTypes) > 0 {
+			return nil, fmt.Errorf("Resolve.Infer: unexpected fields: %v", inferredTypes)
+		}
+
 		// resolving always calls the function
-		// TODO: check args (presence + types)
 		return x.Ret(true), nil
 	}
 	return t, nil
